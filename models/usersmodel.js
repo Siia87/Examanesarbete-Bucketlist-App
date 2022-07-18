85bafd88969aa90a6e96e19549321e53d5f874b8
Sofia Premuzaj <sofia.premuzaj@gmail.com>
2022-07-18 12:00:14 +0200

    Added usersmodel

---
+++ models/usersmodel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+
+  Firstname: {
+    type: String,
+    requiree: true
+  },
+  Lastname: {
+    type: String,
+    requireed: true
+  },
+  Email: {
+    type: String,
+    requireed: true
+  },
+  Password: {
+    type: String,
+    requireed: true
+  },
+},
+  { versionKey: false }
+)
+const Users = mongoose.model('Users', userSchema)
+
+export default Users
